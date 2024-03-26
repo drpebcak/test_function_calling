b8c4a8f318ad6a11ba6276f083160156907f5af5
Yingbei <yingbei@acorn.io>
2024-03-26 16:50:02 -0700

    fix bob

--- gpt-4-turbo-preview/bob.py
+++ gpt-4-turbo-preview/bob.py
@@ -3,8 +3,9 @@ import os, sys
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 from openai_compatible.bob import main
-client = OpenAI(api_key="sk-xx",
-                base_url="https://api.openai.com/v1/")
-                # base_url= "http://localhost:8019/v1/")
+from openai_compatible import API_KEY, BASE_URL
+
+client = OpenAI(api_key=API_KEY,
+                base_url= BASE_URL)
 
 main.run(client, model="gpt-4-turbo-preview")
